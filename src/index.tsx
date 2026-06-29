e72bc60b7384e79b01ebd1eeecca4a797a1150d1
Mustafa Haddara <mustafa@honeycomb.io>
2026-06-29 13:02:38 -0400

    baseurl updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   Link,
   RouterProvider,
+  useNavigate,
   useParams,
-} from 'react-router-dom';
-import { initSDK } from '@embrace-io/web-sdk';
+} from "react-router-dom";
+import { initSDK } from "@embrace-io/web-sdk";
 
-import App from './App';
-import './index.css';
+const App = () => {
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Welcome to the Embrace SDK Demo</h1>
+      <input
+        type="text"
+        placeholder="Type something..."
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          navigate(`/cra-ghpage/name/${name}`);
+        }}
+      >
+        Go to page
+      </button>
+    </div>
+  );
+};
 
 const Name = () => {
   const { name } = useParams();
   return (
     <div>
-      {name} <Link to={'/'}>Home</Link>
+      {name} <Link to={"/cra-ghpage"}>Home</Link>
     </div>
   );
 };
@@ -25,39 +47,39 @@ const Pet = () => {
   return (
     <div>
       {name} has a pet {pet}
-      <Link to={`/name/${name}`}>Back to person</Link>
+      <Link to={`/cra-ghpage/name/${name}`}>Back to person</Link>
     </div>
   );
 };
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/cra-ghpage",
     element: <App />,
   },
   {
-    path: 'name/:name',
+    path: "/cra-ghpage/name/:name",
     element: <Name />,
   },
   {
-    path: 'name/:name/pet/:pet',
+    path: "/cra-ghpage/name/:name/pet/:pet",
     element: <Pet />,
   },
 ]);
 
 try {
   initSDK({
-    appID: 'zbetj',
-    appVersion: '0.0',
+    appID: "zbetj",
+    appVersion: "0.0",
   });
 
-  console.log('Successfully initialized the Embrace SDK');
+  console.log("Successfully initialized the Embrace SDK");
 } catch (err) {
   console.error(err);
 }
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
